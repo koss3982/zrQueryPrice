2464ffa918480b4f4227efe5b8020cdd622e3926
Carles Royan <carles.royan@gmail.com>
2021-09-13 00:09:32 +0200

    Definición de los parámetros de entrada

--- zrQueryPrice-ms/src/main/java/com/croyan/queryprice/rest/controller/QueryController.java
+++ zrQueryPrice-ms/src/main/java/com/croyan/queryprice/rest/controller/QueryController.java
@@ -7,11 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
@@ -28,16 +31,24 @@ public class QueryController {
 
     /**
      * Devuelve el precio de un artículo para una fecha concreta.
+     * @param date Fecha en la cual se quiere saber el precio (obligatorio). Se Espera formato ISO yyyy-MM-dd
+     * @param brandId ID de la marca (obligatorio).
+     * @param  productId ID del producto (obligatorio).
      * @return Devuelve el identificador del artículo y el precio actual de éste.
      */
     @GetMapping(produces = "application/json")
-    public ResponseEntity<ProductPriceBean> getPrice() {
+    public ResponseEntity<ProductPriceBean> getPrice(
+            @RequestParam(value="date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)  LocalDate date,
+            @RequestParam(value="brandId") String brandId,
+            @RequestParam(value="productId") String productId
+    ) {
         ResponseEntity resp;
         ProductPriceBean productPrice = null;
         ServiceResponse.Builder<ProductPriceBean> responseBuilder = new ServiceResponse.Builder();
 
         try {
-            LOG.debug("Llamada getPrice");
+            LOG.debug("Llamada getPrice con los parámetros: " +
+                    "{date: " + date + ", brandId: " + brandId + ", productId: "  + productId + "}");
             // Aquí la parte lógica de la llamada. Esta está delegada al BP correspondiente
             productPrice = queryPriceBP.search(LocalDateTime.now(), 1, 1);
 
